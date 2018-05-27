98a49cd7a4eee64dc15ecc2b0e7ec1b5a685a1e4
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-05-27 22:18:10 +0200

    chrono ok

--- src/pages/launch-list/launch-list.ts
+++ src/pages/launch-list/launch-list.ts
@@ -24,6 +24,8 @@ export class LaunchListPage {
   //Default segment select
   segmentChoice = "past-launches";
 
+  timeForNextLaunch = "00d : 00h : 00m: 00s";
+
   constructor(private navCtrl: NavController, private navParams: NavParams, private spaceXApi : SpacexApiProvider) {
 
     this.spaceXApi.getPastLaunches().subscribe(data=>{
@@ -32,12 +34,40 @@ export class LaunchListPage {
 
     this.spaceXApi.getNextLaunch().subscribe(data=>{
       this.nextLaunch = data;
+      setInterval(this.calculTimeForNExtLaunch.bind(this),1000);
     });
 
     this.spaceXApi.getUpcomingLaunches().subscribe(data=>{
       this.upcomingLaunches = data;
     });
 
+  }
+
+  calculTimeForNExtLaunch(){
+    var launchDate = new Date(this.nextLaunch.launch_date_utc);
+    var now = new Date();
+
+    var timeDiff = Math.abs(now.getTime() - launchDate.getTime());
+    var diffSec = Math.ceil(timeDiff / (1000));
+
+    var nbDays  = parseInt(diffSec / (3600 * 24));
+    var nbDaysInSec = nbDays * 3600 * 24;
+
+    var nbHours = parseInt((diffSec - nbDaysInSec) / (3600));
+    var nbHoursInSec = nbHours * 3600;
+
+    var nbMins  = parseInt((diffSec - nbHoursInSec - nbDaysInSec) / 60);
+    var nbMinsInSec = nbMins * 60;
+
+    var nbSecs = diffSec - nbMinsInSec - nbHoursInSec - nbDaysInSec;
+
+    if(nbSecs == 0 && nbMins == 0 && nbHours == 0 && nbDays){
+      this.spaceXApi.getNextLaunch().subscribe(data=>{
+        this.nextLaunch = data;
+      });
+    }
+
+    this.timeForNextLaunch = nbDays + "d : " + nbHours + "h : " + nbMins + "m : " + nbSecs + "s";
   }
 
   ionViewDidLoad() {
@@ -47,4 +77,5 @@ export class LaunchListPage {
   goToLaunchDetail(launch : ILaunch){
     this.navCtrl.push(LaunchDetailPage, launch);
   }
+
 }
